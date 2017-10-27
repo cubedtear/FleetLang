3ca7075a7793cdb61fe1358f89d5b1d368ae8f12
Aritz Lopez <aritzhack@gmail.com>
2017-10-27 11:44:22 +0200

    Fixed grammar generation
    Windows build disabled until it is tested

--- grammar/build.sh
+++ grammar/build.sh
@@ -1,29 +1,47 @@
 #!/usr/bin/env bash
 
+set -e
+
+resultdir=$1
+grammar=$2
+
+mkdir -p $resultdir
+
+if [ ! -f $grammar ]; then
+    echo "Grammar file $grammar does not exists in $PWD"
+    exit -1
+fi
+
 function get_sha()
 {
-        echo `ls --full-time FleetLang.g4 | sha1sum`
+        echo `ls --full-time ${grammar} | sha1sum`
 }
 
 function get_previous_sha()
 {
-        echo `cat .FleetLang.g4.timestamp` 2>/dev/null || echo ""
+    if [ ! -f ".FleetLang.g4.timestamp" ]; then
+        echo ""
+    else
+        cat .FleetLang.g4.timestamp
+    fi
 }
 
 #printf "Current: %s\n" "$(get_sha)"
 #printf "Old: %s\n" "$(get_previous_sha)"
 
 exit_code=0
-
+pushd ${resultdir} > /dev/null
 if [ "$(get_sha)" != "$(get_previous_sha)" ] ; then
-        java -jar antlr4.jar -Dlanguage=Cpp -no-listener -o ../gen -visitor FleetLang.g4
+        popd > /dev/null
+        java -jar antlr4.jar -Dlanguage=Cpp -no-listener -o ${resultdir} -visitor ${grammar}
         exit_code=$?;
+        pushd ${resultdir} > /dev/null
         echo $(get_sha) > .FleetLang.g4.timestamp
         echo ${exit_code} > ./FleetLang.g4.exitcode
-        echo "------ Grammar updated";
 else
     exit_code=$(cat ./FleetLang.g4.exitcode)
 fi;
+popd > /dev/null
 
 if [[ ${exit_code} != 0 ]]; then
     echo "[EEEE] Error updating grammar!"
